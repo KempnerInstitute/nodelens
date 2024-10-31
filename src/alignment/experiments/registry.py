4744bc0a82aca10e86575de4865c92001619c404
amazloumi <y.mazloomi@gmail.com>
2024-10-31 11:28:02 -0400

    Making experiment_stats working with config file. adding other experiment in the following commits

--- src/alignment/experiments/registry.py
+++ src/alignment/experiments/registry.py
@@ -1,7 +1,8 @@
-from argparse import ArgumentParser
+import sys
 from alignment.experiments.alignment_stats import AlignmentStatistics
 from alignment.experiments.alignment_comparison import AlignmentComparison
 from alignment.experiments.adversarial_shaping import AdversarialShaping
+from alignment.config import ExperimentConfig
 
 EXPERIMENT_REGISTRY = {
     "alignment_stats": AlignmentStatistics,
@@ -9,18 +10,18 @@ EXPERIMENT_REGISTRY = {
     "adversarial_shaping": AdversarialShaping,
 }
 
-def get_experiment(experiment_name, build=False, **kwargs):
+def get_experiment(cfg, build=False):
     """
     lookup model constructor from model registry by name
 
     if build=True, builds experiment and returns an experiment object using any kwargs
     otherwise just returns the class constructor
     """
-    if experiment_name not in EXPERIMENT_REGISTRY:
-        raise ValueError(f"Experiment ({experiment_name}) is not in EXPERIMENT_REGISTRY")
-    experiment = EXPERIMENT_REGISTRY[experiment_name]
+    if cfg.experiment not in EXPERIMENT_REGISTRY:
+        raise ValueError(f"Experiment ({cfg.experiment}) is not in EXPERIMENT_REGISTRY")
+    experiment = EXPERIMENT_REGISTRY[cfg.experiment]
     if build:
-        return experiment(**kwargs)
+        return experiment(cfg)
     return experiment
 
 def create_experiment():
@@ -39,7 +40,11 @@ def create_experiment():
     that is requested rather than showing a help message for this little parser then blocking the
     rest of the execution. It means using --help requires a valid 'experiment' positional argument.
     """
-    parser = ArgumentParser(description=f"ArgumentParser for loading experiment constructor", add_help=False)
-    parser.add_argument("experiment", type=str, help="a string that defines which experiment to run")
-    exp_args, args = parser.parse_known_args()
-    return get_experiment(exp_args.experiment, build=True, args=args)
+    # TODO: adding the command line argument to the config
+    try:
+        yaml_path, args_list = sys.argv[1], sys.argv[2:]
+    except IndexError:
+        raise ValueError(f"Usage: {sys.argv[0]} [CONFIG_PATH] [OPTIONS]")
+
+    cfg = ExperimentConfig.load(yaml_path)
+    return get_experiment(cfg, build=True)
