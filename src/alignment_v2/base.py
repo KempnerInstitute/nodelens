124f1a1e59dbb86f996e61836d3473529c4e26f0
Houman Safaai <houman1359@yahoo.com>
2025-02-03 01:04:28 -0500

    update base.py v2

--- src/alignment_v2/base.py
+++ src/alignment_v2/base.py
@@ -1,96 +1,87 @@
 import torch
 from torch import nn
-from warnings import warn
-from utils import check_iterable, get_unfold_params, get_device, remove_by_idx, set_net_mode, alignment
-
-class AttributeReference:
-    def __init__(self,parent):
-        self.parent=parent
-    def __getattr__(self,name):
-        if hasattr(self.parent,name):
-            return getattr(self.parent,name)
-        raise AttributeError(f"No attr {name} in parent")
+from utils import set_net_mode, default_alignment, get_unfold_params
 
 class AlignmentNetwork(nn.Module):
-    def __init__(self, base_model: nn.Module, alignment_layer_names=None, **kwargs):
+    def __init__(self, base_model: nn.Module, alignment_layer_names=None, alignment_fn=None):
         super().__init__()
-        self.base_model=base_model
-        self.alignment_layers=nn.ModuleList()
-        self.alignment_names=[]
-        self.hidden={}
-        self.hooks={}
-        self.layer_to_input_names=None
+        self.base_model = base_model
+        self.alignment_layers = nn.ModuleList()
+        self.alignment_names = []
+        self.hidden = {}
+        self.hooks = {}
+        self.layer_to_input_names = None
         self._initialize_layers(alignment_layer_names)
+        self.alignment_fn = alignment_fn if alignment_fn is not None else default_alignment
 
     def _initialize_layers(self, aln_names):
         if aln_names is None:
-            for n,m in self.base_model.named_modules():
-                if hasattr(m,"weight"):
-                    self.alignment_layers.append(m)
-                    self.alignment_names.append(n)
+            for name, module in self.base_model.named_modules():
+                if hasattr(module, "weight"):
+                    self.alignment_layers.append(module)
+                    self.alignment_names.append(name)
         else:
-            self.layer_to_input_names={}
-            for n,m in self.base_model.named_modules():
-                if n in aln_names:
-                    if hasattr(m,"weight"):
-                        self.alignment_layers.append(m)
-                        self.alignment_names.append(n)
-                        self.layer_to_input_names[n]=aln_names[n]
+            self.layer_to_input_names = {}
+            for name, module in self.base_model.named_modules():
+                if name in aln_names:
+                    if not hasattr(module, "weight"):
+                        continue
+                    self.alignment_layers.append(module)
+                    self.alignment_names.append(name)
+                    self.layer_to_input_names[name] = aln_names[name]
 
-    def forward(self,x,store_hidden=False):
+    def forward(self, x, store_hidden=False):
         if store_hidden:
-            self.hidden={}
+            self.hidden = {}
             self._setup_hooks()
-        out=self.base_model(x)
+        out = self.base_model(x)
         if store_hidden:
             self._remove_hooks()
         return out
 
     def _setup_hooks(self):
-        def _get_input(n):
-            def hk(mod,inp,out):
-                self.hidden[n]=inp[0]
-            return hk
-        def _get_output(n):
-            def hk(mod,inp,out):
-                self.hidden[n]=out
-            return hk
+        def hook_input(name):
+            def hook(module, inp, out):
+                self.hidden[name] = inp[0]
+            return hook
+        def hook_output(name):
+            def hook(module, inp, out):
+                self.hidden[name] = out
+            return hook
         if self.layer_to_input_names is None:
-            for n,m in zip(self.alignment_names,self.alignment_layers):
-                self.hooks[n]=m.register_forward_hook(_get_input(n))
+            for n, m in zip(self.alignment_names, self.alignment_layers):
+                self.hooks[n] = m.register_forward_hook(hook_input(n))
         else:
-            for n,m in self.base_model.named_modules():
-                if self.layer_to_input_names and n in self.layer_to_input_names.values():
-                    self.hooks[n]=m.register_forward_hook(_get_output(n))
-                elif self.layer_to_input_names and n in self.layer_to_input_names and self.layer_to_input_names[n] is None:
-                    self.hooks[n]=m.register_forward_hook(_get_input(n))
+            for name, module in self.base_model.named_modules():
+                if name in self.layer_to_input_names.values():
+                    self.hooks[name] = module.register_forward_hook(hook_output(name))
+                elif name in self.layer_to_input_names and self.layer_to_input_names[name] is None:
+                    self.hooks[name] = module.register_forward_hook(hook_input(name))
 
     def _remove_hooks(self):
-        for _,h in self.hooks.items():
+        for h in self.hooks.values():
             h.remove()
 
-    def num_layers(self,all=False):
+    def num_layers(self, all=False):
         if all:
-            return sum(1 for n,m in self.base_model.named_modules() if hasattr(m,"weight"))
+            return sum(1 for m in self.base_model.modules() if hasattr(m, "weight"))
         return len(self.alignment_layers)
 
     def is_classification_layer_included(self):
-        named_w=[n for n,m in self.base_model.named_modules() if hasattr(m,"weight")]
-        if not named_w:
-            return False
-        return named_w[-1] in self.alignment_names
+        names = [n for n, m in self.base_model.named_modules() if hasattr(m, "weight")]
+        return names and names[-1] in self.alignment_names
 
     @torch.no_grad()
     def get_layer_inputs(self, x, precomputed=False):
         if not precomputed:
-            _=self.forward(x,store_hidden=True)
-        r=[]
-        for n in self.alignment_names:
-            nm=n
-            if self.layer_to_input_names and self.layer_to_input_names[n] is not None:
-                nm=self.layer_to_input_names[n]
-            r.append(self.hidden[nm])
-        return r
+            _ = self.forward(x, store_hidden=True)
+        outs = []
+        for name in self.alignment_names:
+            key = name
+            if self.layer_to_input_names and self.layer_to_input_names[name] is not None:
+                key = self.layer_to_input_names[name]
+            outs.append(self.hidden[key])
+        return outs
 
     @torch.no_grad()
     def get_alignment_layers(self):
@@ -98,20 +89,17 @@ class AlignmentNetwork(nn.Module):
 
     @torch.no_grad()
     def get_alignment_weights(self, flatten=False):
-        ws=[]
+        weights = []
         for layer in self.alignment_layers:
-            w=layer.weight.data.clone()
+            w = layer.weight.data.clone()
             if flatten:
-                w=w.flatten(start_dim=1)
-            ws.append(w)
-        return ws
+                w = w.flatten(start_dim=1)
+            weights.append(w)
+        return weights
 
     @torch.no_grad()
-    def measure_alignment(self,x,precomputed=False,method="alignment",relative=True):
-        ins=self.get_layer_inputs(x,precomputed)
-        ws=self.get_alignment_weights(flatten=True)
-        out=[]
-        for i,w in zip(ins,ws):
-            out.append(alignment(i,w,method=method,relative=relative))
-        return out
-    # more methods (compare_weights, measure_alignment_weights, forward_targeted_dropout, etc.) can be added as needed
+    def measure_alignment(self, x, precomputed=False, method=None, relative=True):
+        # Use the provided alignment function
+        inputs = self.get_layer_inputs(x, precomputed)
+        weights = self.get_alignment_weights(flatten=True)
+        return [self.alignment_fn(inp, w, relative=relative) for inp, w in zip(inputs, weights)]
