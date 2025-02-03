5f84de60d0481f62acc5c1026e405090350937e0
Houman Safaai <houman1359@yahoo.com>
2025-02-03 13:31:17 -0500

    rename config params v2 - layers

--- src/alignment_v2/models/layers.py
+++ src/alignment_v2/models/layers.py
@@ -1,28 +1,72 @@
 from torch import nn
+from alignment import utils
 
-REGISTRY_REQUIREMENTS = ["name", "layer_index", "unfold", "ignore", "flag"]
+# The LAYER_REGISTRY contains meta parameters for each type of layer used in alignment networks
+REGISTRY_REQUIREMENTS = [
+    "name",
+    "layer_index",
+    "unfold",
+    "ignore",
+    "flag",
+]
 
 LAYER_REGISTRY = {
-    nn.Linear: {"name": "linear", "layer_index": None, "unfold": False, "ignore": False, "flag": False},
-    nn.Conv2d: {"name": "conv2d", "layer_index": None, "unfold": True, "ignore": False, "flag": True},
+    nn.Linear: {
+        "name": "linear",
+        "layer_index": None,
+        "unfold": False,
+        "ignore": False,
+        "flag": False,
+    },
+    nn.Conv2d: {
+        "name": "conv2d",
+        "layer_index": None,
+        "unfold": True,
+        "ignore": False,
+        "flag": True,
+    },
 }
 
 def default_metaprms_ignore(name):
-    return {"name": name, "layer_index": None, "unfold": False, "ignore": True, "flag": True}
+    metaparameters = {
+        "name": name,
+        "layer_index": None,
+        "unfold": False,
+        "ignore": True,
+        "flag": True,
+    }
+    return metaparameters
 
 def default_metaprms_linear(index, name="linear", flag=False):
-    return {"name": name, "layer_index": index, "unfold": False, "ignore": False, "flag": flag}
+    metaparameters = {
+        "name": name,
+        "layer_index": index,
+        "unfold": False,
+        "ignore": False,
+        "flag": flag,
+    }
+    return metaparameters
 
 def default_metaprms_conv2d(index, name="conv2d", flag=True):
-    return {"name": name, "layer_index": index, "unfold": True, "ignore": False, "flag": flag}
+    metaparameters = {
+        "name": name,
+        "layer_index": index,
+        "unfold": True,
+        "ignore": False,
+        "flag": flag,
+    }
+    return metaparameters
 
 def check_metaparameters(metaparameters, throw=True):
-    for key in REGISTRY_REQUIREMENTS:
-        if key not in metaparameters:
-            if throw:
-                raise ValueError(f"Missing key: {key}")
-            return False
+    if not all([required in metaparameters for required in REGISTRY_REQUIREMENTS]):
+        if throw:
+            raise ValueError(f"metaparameters are missing required keys, it requires all of the following: {REGISTRY_REQUIREMENTS}")
+        return False
     return True
 
-for lt, mp in LAYER_REGISTRY.items():
-    check_metaparameters(mp)
+for layer_type, metaparameters in LAYER_REGISTRY.items():
+    if not check_metaparameters(metaparameters, throw=False):
+        raise ValueError(
+            f"Layer type: {layer_type} from the `LAYER_REGISTRY` is missing metaparameters. "
+            f"It requires all of the following: {REGISTRY_REQUIREMENTS}"
+        )
