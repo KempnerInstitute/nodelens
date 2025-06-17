5d0165cfb057f7d2fcdeb37cb0ef49b7b3acf19a
Houman Safaai <houman1359@yahoo.com>
2025-06-17 10:12:37 -0400

    rename refactored folder

--- src/alignment/utils/__init__.py
+++ src/alignment/utils/__init__.py
@@ -1,81 +1,51 @@
 """
-Utilities for the alignment package.
-
-This module contains various utility functions for the alignment package,
-including plotting, timing, linear algebra helpers, and other utility functions.
+Utility functions and helpers for the alignment framework.
 """
 
-# Import plotting functions
-from alignment.utils.plotting import (
-    plot_dropout_results,
-    plot_experiment_summary,
-    plot_dropout_comparison,
-    log_plots_to_wandb
+from alignment_refactor.utils.distributed import (
+    setup_distributed,
+    cleanup_distributed,
+    is_main_process,
+    barrier,
+    reduce_tensor,
+    gather_tensor,
 )
-
-# Import core functions to maintain backward compatibility
-from alignment.utils.core import (
+from alignment_refactor.utils.checkpoint import (
+    save_checkpoint,
+    load_checkpoint,
+    CheckpointManager,
+)
+from alignment_refactor.utils.logging import (
     setup_logging,
-    timer,
-    debug,
-    to_numpy,
-    to_tensor,
-    check_iterable,
-    ensure_device,
-    timed
+    get_logger,
+    log_metrics,
 )
-
-# Import math utilities
-from alignment.utils.math import (
-    orthogonalize,
-    compute_correlation_matrix,
-    matrix_angles,
-    project_to_subspace
+from alignment_refactor.utils.config import (
+    load_config,
+    save_config,
+    merge_configs,
+    Config,
 )
-
-# Import model utilities
-from alignment.utils.model_utils import (
-    get_device,
-    set_net_mode,
-    get_maximum_strides,
-    get_unfold_params,
-    weighted_average,
-    remove_by_idx,
-    smart_pca
-)
-
-# Removed deprecated plotting functions: plot_pruning_experiments, plot_per_layer_independent
-# They were stubs printing warnings. Their functionality should be covered by other plotting utilities.
 
 __all__ = [
-    # Plotting functions
-    'plot_dropout_results',
-    'plot_experiment_summary',
-    'plot_dropout_comparison',
-    'log_plots_to_wandb',
-    
-    # Core utilities
+    # Distributed utilities
+    'setup_distributed',
+    'cleanup_distributed',
+    'is_main_process',
+    'barrier',
+    'reduce_tensor',
+    'gather_tensor',
+    # Checkpoint utilities
+    'save_checkpoint',
+    'load_checkpoint',
+    'CheckpointManager',
+    # Logging utilities
     'setup_logging',
-    'timer',
-    'debug',
-    'to_numpy',
-    'to_tensor',
-    'check_iterable',
-    'ensure_device',
-    'timed',
-    
-    # Math utilities
-    'orthogonalize',
-    'compute_correlation_matrix',
-    'matrix_angles',
-    'project_to_subspace',
-    
-    # Model utilities
-    'get_device',
-    'set_net_mode',
-    'get_maximum_strides',
-    'get_unfold_params',
-    'weighted_average',
-    'remove_by_idx',
-    'smart_pca'
-]
+    'get_logger',
+    'log_metrics',
+    # Config utilities
+    'load_config',
+    'save_config',
+    'merge_configs',
+    'Config',
+] 
