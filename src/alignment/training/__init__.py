1b8520127e32cdc68c863205b12a9b91df2b8452
Houman Safaai <houman1359@yahoo.com>
2025-09-09 14:36:37 -0400

    Add remaining training module files
    - Updated training module documentation
    - Enhanced training module initialization

--- src/alignment/training/__init__.py
+++ src/alignment/training/__init__.py
@@ -4,10 +4,13 @@ Training utilities for neural networks.
 
 from .base import BaseTrainer, TrainingConfig
 from .multi_network import train_networks_fully_tensorized, TensorizedNetworkWrapper
+from .experiment_trainer import ExperimentTrainer, ExperimentTrainingConfig
 
 __all__ = [
     'BaseTrainer',
     'TrainingConfig',
     'train_networks_fully_tensorized',
     'TensorizedNetworkWrapper',
+    'ExperimentTrainer',
+    'ExperimentTrainingConfig',
 ] 
